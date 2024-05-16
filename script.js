2d79eeff00b17901c885ac91e164ca83c20e4956
ThiagoSousa81 <thiagosousa81@gmail.com>
2024-05-15 22:38:24 -0300

    §

--- script.js
+++ script.js
@@ -25,6 +25,49 @@ document.getElementById("frmPassagens").addEventListener("submit", function(even
     console.log("Quantidade de passagens:", quantidadePassagens);
     console.log("Classe:", classe);
     console.log("Passagens de ida e volta:", idaVolta);
-    
-    // Aqui você pode realizar qualquer outra operação com os dados, como enviar para um servidor via AJAX, por exemplo
 });
+
+let db = {
+    cidades: [
+        {
+            nome: "São Felipe",
+            idaVolta: "Sim"
+        },
+        {
+            nome: "Mombaça",
+            idaVolta: "Não"
+        },
+        {
+            nome: "Conceição do Almeida",
+            idaVolta: "Sim"
+        },
+        {
+            nome: "Sapeaçu",
+            idaVolta: "Não"
+        },
+        {
+            nome: "Cruz das Almas",
+            idaVolta: "Sim"
+        },
+        {
+            nome: "Gov. Mangabeira",
+            idaVolta: "Não"
+        },
+        {
+            nome: "São Félix",
+            idaVolta: "Não"
+        },
+        {
+            nome: "Salvador",
+            idaVolta: "Sim"
+        }
+    ]
+}
+
+while (i < cidades.length) {
+    console.log(numeros[i]);
+    i++;
+}
+// Aqui você pode realizar qualquer outra operação com os dados, como enviar para um servidor via AJAX, por exemplo
+
+
